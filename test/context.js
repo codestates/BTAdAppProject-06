632ea2a0851029e257f39dd93313341d14a88a06
cothi <jiungdev@gmail.com>
2022-10-28 22:12:53 +0900

    feat: import web3

--- test/context.js
+++ test/context.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import Web3 from 'web3';
-import Pay from './utils/Pay.json'
+import React from "react";
+import Web3 from "web3";
+import Pay from "./utils/Pay.json";
 
-let web3 = new Web3("https://api.baobab.klaytn.net:8651")
-let contract = new web3.eth.Contract(Pay.abi, Pay.deployedAddress)
-export const web3Context = React.createContext({web3, contract});
+let web3 = new Web3("https://api.baobab.klaytn.net:8651");
+
+let contract = new web3.eth.Contract(Pay.abi, Pay.deployedAddress);
+export const web3Context = React.createContext({ web3, contract });
