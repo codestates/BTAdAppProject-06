09a3d38004b5a1a9ba1d364f14c2e4965edc6b84
HyD3c <dlscjf151@naver.com>
2022-10-29 00:31:46 +0900

    test: web3 wallet 암호화 추가

--- test/pages/ImportAccount.js
+++ test/pages/ImportAccount.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, TextInput, Button} from "react-native";
+import "react-native-get-random-values";
 import {web3Context} from "../context";
 
 function ImportAccount() {
+    const {web3} = useContext(web3Context);
     const [text, onChangeText] = React.useState("Useless Text");
+    const [encryptedWallet, changeEncryptedWallet] = React.useState();
     return (
         <View>
             <TextInput
@@ -11,12 +14,18 @@ function ImportAccount() {
                 value={text}
             />
             <Button title="추가" onPress={() => {
-                web3Context._currentValue.web3.eth.accounts.wallet.add(text)
-                console.log(web3Context._currentValue.web3.eth.accounts.wallet)
+                web3.eth.accounts.wallet.add(text)
+                let enc = web3.eth.accounts.wallet.encrypt("password")
+                changeEncryptedWallet(enc)
+                console.log(web3.eth.accounts.wallet.decrypt(enc, "password"))
+                console.log(web3.eth.accounts.wallet)
             }}></Button>
             <web3Context.Consumer>
                 {value => <Text>{value.web3.eth.accounts.wallet.length.toString()}</Text>}
             </web3Context.Consumer>
+            <Text>
+                Encrypted Wallet : {JSON.stringify(encryptedWallet)}
+            </Text>
         </View>
     )
 }
