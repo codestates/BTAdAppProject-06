81ee751d9896ed6afbb32a4b89c748c4435f8724
cothi <jiungdev@gmail.com>
2022-10-29 00:46:50 +0900

    fix: contract error

--- dapp-frontend/context.js
+++ dapp-frontend/context.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import Web3 from "web3";
-import Pay from "./utils/Pay.json";
+//import Pay from "./utils/Pay.json";
 
 // provider
 export const DBContext = React.createContext();
@@ -9,6 +9,6 @@ export const useDB = () => {
 };
 
 // web3
-let web3 = new Web3("https://api.baobab.klaytn.net:8651");
-let contract = new web3.eth.Contract(Pay);
-export const web3Context = React.createContext({ web3, contract });
+//let web3 = new Web3("https://api.baobab.klaytn.net:8651");
+//let contract = new web3.eth.Contract(Pay);
+//export const web3Context = React.createContext({ web3, contract });
